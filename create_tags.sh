4b63a5546d3b253de8bc75cdf6f77e78fc664c9d
David Pappe <fpappe@hotmail.com>
2022-08-19 08:15:00 -0400

    feat.dav-jenkins  test1

--- create_tags.sh
+++ create_tags.sh
@@ -1,7 +1,9 @@
-# Publish on github
+# Build
+# "make all" for example
 
+# Publish on github
 echo "Publishing on Github..."
-token="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"
+token="<YOUR GITHUB ACCESS TOKEN>"
 
 # Get the last tag name
 tag=$(git describe --tags)
@@ -15,19 +17,10 @@ description=$(echo "$message" | tail -n +3)
 description=$(echo "$description" | sed -z 's/\n/\\n/g') # Escape line breaks to prevent json parsing problems
 
 # Create a release
-release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"master\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/dafrpappe/sample-python/releases)
+release=$(curl -XPOST -H "Authorization:token $token" --data "{\"tag_name\": \"$tag\", \"target_commitish\": \"master\", \"name\": \"$name\", \"body\": \"$description\", \"draft\": false, \"prerelease\": true}" https://api.github.com/repos/<USERNAME>/<REPOSITORY>/releases)
 
 # Extract the id of the release from the creation response
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 
 # Upload the artifact
-curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @artifact.zip https://uploads.github.com/repos/dafrpappe/sample-python/releases/$id/assets?name=artifact.zip
-
-
-+refs/heads/master:refs/remotes/origin/master +refs/heads/develop:refs/remotes/origin/develop
-+refs/tags/main:refs/remotes/origin/tags/*
-
-+refs/tags/*:refs/remotes/origin/tags/*
-+refs/tags/*:refs/remotes/origin/tags/*
-
-refs/tags/*
+curl -XPOST -H "Authorization:token $token" -H "Content-Type:application/octet-stream" --data-binary @artifact.zip https://uploads.github.com/repos/<USERNAME>/<REPOSITORY>/releases/$id/assets?name=artifact.zip
